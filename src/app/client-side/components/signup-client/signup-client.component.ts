35ede7d24cf4147ff0775d8618ddb245590a1489
mhaitham <mhaitham@sumerge.com>
2021-05-04 00:09:18 +0200

    added signup

--- src/app/client-side/components/signup-client/signup-client.component.ts
+++ src/app/client-side/components/signup-client/signup-client.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ResPatient } from 'src/app/interfaces/res-patient';
+import { ServPatientService } from 'src/app/services/serv-patient.service';
 
 @Component({
   selector: 'app-signup-client',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupClientComponent implements OnInit {
 
-  constructor() { }
+  //@ViewChild('newPatientForm', { static: false }) newPatientForm: NgForm;
+
+  constructor(
+  private servPatient:ServPatientService) {
+   
+  }
 
   ngOnInit(): void {
   }
 
+  
+// onNewPatientSubmit(){
+//   const newPatient:ResPatient ={
+//     code:0, //code must ALWAYS be 0 when inserting bec. it wont insert if code is existing and 
+//     //it starts from 1 so 0 always available
+//     name:this.newPatientForm.value.name,
+//     gender:this.newPatientForm.value.gender,
+//     age:this.newPatientForm.value.age,
+//     mobile:this.newPatientForm.value.mobile,
+//     email:this.newPatientForm.value.email,
+//     totalDebt:0,
+//     notes:"",
+//     appointments:[],
+//     medImages:[],
+//     memberships:[]
+//   }
+  
+//   this.servPatient.addPatient(newPatient).subscribe(response =>{
+//   })
+// }
 }
