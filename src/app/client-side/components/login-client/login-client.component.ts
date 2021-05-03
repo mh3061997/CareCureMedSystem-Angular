599a216d8e8a1e62d217d1a397c2a4f3d67bb2c8
mhaitham <mhaitham@sumerge.com>
2021-05-04 00:05:22 +0200

    centered bannerhead bg and aded spiner for login

--- src/app/client-side/components/login-client/login-client.component.ts
+++ src/app/client-side/components/login-client/login-client.component.ts
@@ -21,28 +21,51 @@ export class LoginClientComponent implements OnInit {
   showSpinner=false
 
   @Input() error: string | null;
+  constructor(private router: Router,
+    private authService: AuthService) { }
 
-  constructor( private loginservice:AuthService, private router: Router) { }
-
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
   checkLogin() {
-    this.showSpinner=true;
-    (this.loginservice.authenticate(this.username, this.password).subscribe(
-      data => {
-        this.router.navigate(['admin','patient'])
+    this.showSpinner = true;
+    (this.authService.authenticate(this.username, this.password).subscribe(
+      success => {
         this.invalidLogin = false
-        this.showSpinner=false;
+        this.showSpinner = false;
+        this.redirectToResponsiblePage();
       },
       error => {
         this.invalidLogin = true
         this.error = error.error.message;
-        this.showSpinner=false;
+        this.showSpinner = false;
 
       }
     )
     );
+
+  }
+
+  redirectToResponsiblePage() {
+    if (this.authService.isUserReceptionist()) {
+
+      this.router.navigate(['admin', 'appointment']);
+
+    } else if (this.authService.isUserDoctor()) {
+      const doctorCode = this.authService.getLoggedInDoctorCode();
+      this.router.navigate(['admin', 'doctor',doctorCode]);
+
+
+    } else if (this.authService.isUserAdmin()) {
+
+      this.router.navigate(['admin', 'appointment']);
+
+
+    } else if (this.authService.isUserPatient()) {
+
+      this.router.navigate(['history']);
+
+    }
 
   }
 }
--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -12,28 +12,28 @@ export class LoginComponent implements OnInit {
   username = ''
   password = ''
   invalidLogin = false
-  showSpinner=false
+  showSpinner = false
 
   @Input() error: string | null;
 
   constructor(private router: Router,
-    private loginservice: AuthService) { }
+    private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   checkLogin() {
-    this.showSpinner=true;
-    (this.loginservice.authenticate(this.username, this.password).subscribe(
-      data => {
-        this.router.navigate(['admin','patient'])
+    this.showSpinner = true;
+    (this.authService.authenticate(this.username, this.password).subscribe(
+      success => {
         this.invalidLogin = false
-        this.showSpinner=false;
+        this.showSpinner = false;
+        this.redirectToResponsiblePage();
       },
       error => {
         this.invalidLogin = true
         this.error = error.error.message;
-        this.showSpinner=false;
+        this.showSpinner = false;
 
       }
     )
@@ -41,4 +41,28 @@ export class LoginComponent implements OnInit {
 
   }
 
+  redirectToResponsiblePage() {
+    if (this.authService.isUserReceptionist()) {
+
+      this.router.navigate(['admin', 'appointment']);
+
+    } else if (this.authService.isUserDoctor()) {
+      const doctorCode = this.authService.getLoggedInDoctorCode();
+      this.router.navigate(['admin', 'doctor',doctorCode]);
+
+
+    } else if (this.authService.isUserAdmin()) {
+
+      this.router.navigate(['admin', 'appointment']);
+
+
+    } else if (this.authService.isUserPatient()) {
+
+      this.router.navigate(['history']);
+
+    }
+
+  }
+
 }
+
