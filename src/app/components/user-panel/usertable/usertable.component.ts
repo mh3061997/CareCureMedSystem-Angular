247eaeb10788209bd88f76e372d578163455f81b
mhaitham <mhaitham@sumerge.com>
2021-05-08 06:15:34 +0200

    Added Search for users table

--- src/app/components/user-panel/usertable/usertable.component.ts
+++ src/app/components/user-panel/usertable/usertable.component.ts
@@ -1,7 +1,7 @@
-import {AfterViewInit, ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges, ViewChild} from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { AfterViewInit, ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { ResUserDao } from 'src/app/interfaces/res-user-dao';
 import { UserChangeRoleDialogComponent } from './user-change-role-dialog/user-change-role-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
@@ -15,14 +15,14 @@ import { UserEnableDisableDialogComponent } from './user-enable-disable-dialog/u
 })
 export class UsertableComponent implements AfterViewInit, OnChanges {
 
-  
+
   @Input()
-  users:ResUserDao[];
+  users: ResUserDao[];
 
 
 
-  
-  displayedColumns: string[] = ['code', 'username', 'role', 'name','enabled',' '];
+
+  displayedColumns: string[] = ['code', 'username', 'role', 'name', 'enabled', ' '];
 
   dataSource: MatTableDataSource<ResUserDao>;
 
@@ -31,19 +31,15 @@ export class UsertableComponent implements AfterViewInit, OnChanges {
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(private cdr: ChangeDetectorRef,
-    private dialogChangeUserRole:MatDialog,
-    private dialogEnableDisableUser:MatDialog,
-    private servUsers:ServUsersService){
+    private dialogChangeUserRole: MatDialog,
+    private dialogEnableDisableUser: MatDialog,
+    private servUsers: ServUsersService) {
 
   }
-  
+
   ngAfterViewInit() {
-  
-           // Assign the data to the data source for the table to render
-           this.dataSource = new MatTableDataSource(this.users);
-           this.dataSource.paginator = this.paginator;
-           this.dataSource.sort = this.sort;
-     // If the user changes the sort order, reset back to the first page.
+
+    // If the user changes the sort order, reset back to the first page.
     this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
     this.cdr.detectChanges();
 
@@ -59,75 +55,129 @@ export class UsertableComponent implements AfterViewInit, OnChanges {
   }
 
 
-  
+
   ngOnChanges(changes: SimpleChanges) {
     // only run when property "data" changed
     if (changes['users']) {
       this.dataSource = new MatTableDataSource(this.users);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      this.dataSource.filterPredicate =
+        (user: ResUserDao, filter: string) => {
+          return user.name != null && user.name.trim().toLowerCase().indexOf(filter) != -1;
+        };
+
     }
-}
+  }
 
-openChangeRoleDialog(user:ResUserDao){
+  onSearchFilterChange(searchFilter:string){
 
-  const dialogRef =  this.dialogChangeUserRole.open(UserChangeRoleDialogComponent,{
-    data:{
-      user:user
-    },
-    disableClose:true
-  });
+  if(searchFilter === 'Name'){
 
-  dialogRef.afterClosed().subscribe(closed =>{
- 
-   if(closed){
-    this.servUsers.getUsersAll().subscribe(users =>{
+    this.dataSource.filterPredicate =
+    (user: ResUserDao, filter: string) => {
+      return user.name != null && user.name.trim().toLowerCase().indexOf(filter) != -1;
+    };
 
-      this.users = users;
-      });
-   }
+  }else if(searchFilter === 'Code') {
 
-  });
+    this.dataSource.filterPredicate =
+    (user: ResUserDao, filter: string) => {
+      return user.code != null && user.code.toString() == filter;
+    };
 
-}
+  }else if(searchFilter === 'Username') {
 
-openEnableDisableDialog(user:ResUserDao,enable:boolean){
-
-  const dialogRef =  this.dialogEnableDisableUser.open(UserEnableDisableDialogComponent,{
-    data:{
-      user:user,
-      enable:enable
-    },
-    disableClose:true
-  });
-
-  dialogRef.afterClosed().subscribe(closed =>{
- 
-   if(closed){
-    this.servUsers.getUsersAll().subscribe(users =>{
+    this.dataSource.filterPredicate =
+    (user: ResUserDao, filter: string) => {
+      return user.username != null && user.username.trim().toLowerCase().indexOf(filter) != -1;
+    };
     
-      this.users = users;
-      });
-   }
+  }else if(searchFilter === 'Role') {
 
-  });
+    this.dataSource.filterPredicate =
+    (user: ResUserDao, filter: string) => {
+      return user.roles[0] != null && user.roles[0].name.trim().toLowerCase().indexOf(filter) != -1;
+    };
+    
+  }else if(searchFilter === 'Enabled') {
+
+    this.dataSource.filterPredicate =
+    (user: ResUserDao, filter: string) => {
+      if(filter.trim().toLowerCase() === "yes"){
+
+        return user.enabled;
+
+      }
+      else return !user.enabled;
+      
+      
+    };
+    
+  }
+
+  }
+  openChangeRoleDialog(user: ResUserDao) {
+
+    const dialogRef = this.dialogChangeUserRole.open(UserChangeRoleDialogComponent, {
+      data: {
+        user: user
+      },
+      disableClose: true
+    });
+
+    dialogRef.afterClosed().subscribe(closed => {
+
+      if (closed) {
+        this.servUsers.getUsersAll().subscribe(users => {
+
+          this.users = users;
+        });
+      }
+
+    });
+
+  }
+
+  openEnableDisableDialog(user: ResUserDao, enable: boolean) {
+
+    const dialogRef = this.dialogEnableDisableUser.open(UserEnableDisableDialogComponent, {
+      data: {
+        user: user,
+        enable: enable
+      },
+      disableClose: true
+    });
+
+    dialogRef.afterClosed().subscribe(closed => {
+
+      if (closed) {
+        this.servUsers.getUsersAll().subscribe(users => {
+
+          this.users = users;
+        });
+      }
+
+    });
+
+
+  }
 
 
 }
 
 
-}
 
 
 
 
 
-  
 
 
 
 
 
- 
-   
+
 
 
 
