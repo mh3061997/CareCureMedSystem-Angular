e945ffcabde619a448040733e75dac3fbd4e6c61
mhaitham <mhaitham@sumerge.com>
2021-05-14 16:11:23 +0200

    changed prod api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiEndPoint: "https://carecureclinics.com:8080/medsystem-1.0/",
-  apiBlogEndPoint: "https://blog.carecureclinics.com:80/"
+  apiEndPoint: "https://carecureclinics.com:8443/medsystem-1.0/",
+  apiBlogEndPoint: "https://blog.carecureclinics.com/"
 };
