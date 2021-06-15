0a23a3540c3c253f8229201dc3f7a11d3d9d02cf
mhaitham <mhaitham@sumerge.com>
2021-06-15 23:30:24 +0200

    Added Kebab Menu prototype

---
+++ src/app/components/inventory/inventory-item-table/kebab-item-table/kebab-item-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kebab-item-table',
+  templateUrl: './kebab-item-table.component.html',
+  styleUrls: ['./kebab-item-table.component.css']
+})
+export class KebabItemTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
