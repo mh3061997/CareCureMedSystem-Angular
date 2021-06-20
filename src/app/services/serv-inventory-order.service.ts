1f8fd550cedcaf42b6d851647b2834db78222dd4
mhaitham <mhaitham@sumerge.com>
2021-06-20 22:10:02 +0200

    Added Invnetory order FIltering,sorting,paging

--- src/app/services/serv-inventory-order.service.ts
+++ src/app/services/serv-inventory-order.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { DtoInventoryOrderNew } from '../dtos/dto-inventory-order-new';
+import { EnumInventoryOrderType } from '../enums/enum-inventory-order-type.enum';
 import { ResInventoryOrder } from '../interfaces/inventory/res-inventory-order';
 import { PathService } from './path.service';
 
@@ -15,24 +16,40 @@ export class ServInventoryOrderService {
 
   path = this.servPath.getPathinventoryOrder();
 
-  getOrders(pageNumber: number, pageSize: number, sortColumn: string, sortDirection: string) {
+  getOrders(pageNumber: number, pageSize: number, sortColumn: string, sortDirection: string, startDate: Date | null, endDate: Date | null, orderType: EnumInventoryOrderType | null) {
+
+    let typeString = orderType?.toString(),
+      startDateString = startDate?.toISOString(),
+      endDateString = endDate?.toISOString();
+
+      let httpParams:any = {
+        
+          pageNumber: pageNumber.toString(),
+          pageSize: pageSize.toString(),
+          sortColumn: sortColumn,
+          sortDirection: sortDirection,
+
+      };
+      if(typeString)
+      httpParams.type = typeString;
+      if(startDateString)
+      httpParams.startDate = startDateString;
+      if(endDateString)
+      httpParams.endDate = endDateString;
+      
     return this.http.get<any>(this.path, {
-      params: {
-        pageNumber: pageNumber.toString(),
-        pageSize: pageSize.toString(),
-        sortColumn: sortColumn,
-        sortDirection: sortDirection
-      },
-      observe:'response'
+      params: httpParams,
+      observe: 'response'
     });
   }
 
+
   addOrder(newOrder: DtoInventoryOrderNew) {
-    return this.http.post<ResInventoryOrder>(this.path,newOrder);
+    return this.http.post<ResInventoryOrder>(this.path, newOrder);
   }
 
-  reverseOrder(Ordercode:number){
-    return this.http.delete(this.path+Ordercode);
+  reverseOrder(Ordercode: number) {
+    return this.http.delete(this.path + Ordercode);
   }
 
 
