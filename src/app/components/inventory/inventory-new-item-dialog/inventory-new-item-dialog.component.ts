0062ed991179bfb7840319e2d9b69619af362e02
mhaitham <mhaitham@sumerge.com>
2021-06-23 01:03:44 +0200

    Added new Item Dialog

--- src/app/components/inventory/inventory-new-item-dialog/inventory-new-item-dialog.component.ts
+++ src/app/components/inventory/inventory-new-item-dialog/inventory-new-item-dialog.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { ServInventoryItemService } from 'src/app/services/serv-inventory-item.service';
+import { DtoInventoryItemNew } from 'src/app/dtos/dto-inventory-item-new';
+import { EnumInventoryItemCategory } from 'src/app/enums/enum-inventory-item-category.enum';
 
 @Component({
   selector: 'app-inventory-new-item-dialog',
@@ -7,8 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventoryNewItemDialogComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('newItemForm', { static: false }) newItemForm: NgForm;
 
+
+  constructor(private dialogRef: MatDialogRef<InventoryNewItemDialogComponent>,
+    private servInventoryItem: ServInventoryItemService) {
+
+    this.disableBackClick();
+
+  }
+
+
+  disableBackClick() {
+    this.dialogRef.backdropClick().subscribe(() => {
+      this.dialogRef.close(false);
+    });
+  }
+
+  onNewInventoryItemSubmit() {
+    console.log(this.newItemForm.value);
+
+    const newItem = new DtoInventoryItemNew(this.newItemForm.value.name,
+      this.newItemForm.value.category, this.newItemForm.value.price);
+
+      console.log(newItem);
+
+      this.servInventoryItem.addInventoryItem(newItem).subscribe(()=>{
+        this.servInventoryItem.emitItemsUpdatedSubject();
+        this.dialogRef.close();
+      });
+      
+  }
+
+  getInventoryItemCategories(){
+    return Object.values(EnumInventoryItemCategory);
+  }
   ngOnInit(): void {
   }
 
--- src/app/components/inventory/inventory-page/inventory-page.component.ts
+++ src/app/components/inventory/inventory-page/inventory-page.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { ResInventoryItem } from 'src/app/interfaces/inventory/res-inventory-item';
 import { ResInventoryOrder } from 'src/app/interfaces/inventory/res-inventory-order';
 import { ServInventoryItemService } from 'src/app/services/serv-inventory-item.service';
 import { ServInventoryOrderService } from 'src/app/services/serv-inventory-order.service';
+import { InventoryNewItemDialogComponent } from '../inventory-new-item-dialog/inventory-new-item-dialog.component';
+import { InventoryNewOrderDialogComponent } from '../inventory-new-order-dialog/inventory-new-order-dialog.component';
 
 @Component({
   selector: 'app-inventory-page',
@@ -14,10 +17,25 @@ import { ServInventoryOrderService } from 'src/app/services/serv-inventory-order
 export class InventoryPageComponent implements OnInit {
 
 
-  constructor(private cdr: ChangeDetectorRef) { }
+  constructor(private cdr: ChangeDetectorRef,
+    private dialogAddNewInventoryItem: MatDialog,
+    private dialogAddNewInventoryOrder: MatDialog) { }
 
   ngOnInit(): void {
 
+  }
+
+  openAddNewInventoryItemDialog() {
+    this.dialogAddNewInventoryItem.open(InventoryNewItemDialogComponent, { disableClose: true });
+  }
+
+  openAddNewInventoryOrderDialog(isSupply: boolean) {
+    this.dialogAddNewInventoryOrder.open(InventoryNewOrderDialogComponent, {
+      data: {
+        isSupply: isSupply
+      }, 
+      disableClose: true
+    });
   }
 
 }
