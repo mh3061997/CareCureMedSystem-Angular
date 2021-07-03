bc71d8a2a1879c9c1f8335aee35b30a44df029ff
mhaitham <mhaitham@sumerge.com>
2021-07-03 17:29:00 +0200

    Added Reports Http

--- src/app/components/reports-page/reports-page.component.ts
+++ src/app/components/reports-page/reports-page.component.ts
@@ -3,6 +3,8 @@ import * as Highstock from "highcharts/highstock";
 import { Options, SeriesOptionsType } from "highcharts";
 import HC_exporting from "highcharts/modules/exporting";
 import HC_exporting_DATA from "highcharts/modules/export-data";
+import { HttpClient } from '@angular/common/http';
+import { ServUtilitiesService } from 'src/app/services/serv-utilities.service';
 
 HC_exporting(Highstock);
 HC_exporting_DATA(Highstock);
@@ -14,121 +16,119 @@ HC_exporting_DATA(Highstock);
 })
 export class ReportsPageComponent implements OnInit {
 
-   data = [{
-      name: 'Surgery',
-      data: [677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654, 677, 500, 700, 555, 444, 777, 877, 944, 567, 666, 789, 456, 654, 677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654]
-
-
-   }, {
-      name: 'Dermatology',
-      data: [677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654, 677, 677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654, 677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654]
-
-   }, {
-      name: 'Plastic Surgery',
-      data: [677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654, 677, 677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654, 677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654]
-
-   }, {
-      name: 'Nutrition',
-      data: [677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654, 677, 677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654, 677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654]
-
-   }, {
-      name: 'Internal Medicine',
-      data: [677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654, 677, 677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654, 677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654]
-
-   }, {
-      name: 'Obstetrics',
-      data: [677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654, 677, 677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654, 677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654]
-
-   }, {
-      name: 'Dentistry',
-      data: [677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654, 677, 677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654, 677, 455, 677, 877, 455, 778, 888, 567, 785, 488, 567, 654]
-
-   },];
-
-
+   monthlySpecialitiesIncome: any[];
+   rebased:any[];
    highcharts = Highstock;
 
-   chartOptions: Options = {
-      exporting: {
-         enabled: true,
-         chartOptions: { // specific options for the exported image
-            plotOptions: {
-               series: {
-                  dataLabels: {
-                     enabled: false
+   chartOptions: Options;
+   constructor(private http: HttpClient, private servUtils: ServUtilitiesService) {
+
+      this.http.get<any>("http://localhost:8080/reports/monthlySpecialitiesIncome").subscribe((data) => {
+         this.monthlySpecialitiesIncome = data;
+         console.log(this.monthlySpecialitiesIncome);
+         this.rebased = this.createArray();
+        console.log(this.rebased);
+        
+
+         this.chartOptions = {
+            exporting: {
+               enabled: true,
+               chartOptions: { // specific options for the exported image
+                  plotOptions: {
+                     series: {
+                        dataLabels: {
+                           enabled: false
+                        }
+                     }
                   }
-               }
-            }
-         },
-         scale: 1,
-         fallbackToExportServer: true
-      },
-      tooltip: {
-         headerFormat: '<span style="font-size:20px;">{point.key}</span><table style="">',
-         pointFormat: '<tr><td style="font-size:15px;font-weight:bold;color:{series.color};padding:0">{series.name}</td>' +
-             '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
-         footerFormat: '</table>',
-         shared: true,
-         useHTML: true
-     },
-      chart: {
-         type: "column", zoomType: 'x'
-      },
-      title: {
-         text: "Monthly Specialities Income"
-      },
-
-      xAxis: {
-         // zoomEnabled:true,
-         scrollbar: { enabled: true, minWidth: 800 },
-         categories: [
-            "Jan-2017", "Feb-2017", "Mar-2017", "Apr-2017", "May-2017", "Jun-2017", "Jul-2017", "Aug-2017", "Sep-2017", "Oct-2017", "Nov-2017", "Dec-2017",
-            "Jan-2021", "Feb-2021", "Mar-2021", "Apr-2021", "May-2021", "Jun-2021", "Jul-2021", "Aug-2021", "Sep-2021", "Oct-2021", "Nov-2021", "Dec-2021",
-            "Jan-2022", "Feb-2022", "Mar-2022", "Apr-2022", "May-2022", "Jun-2022", "Jul-2022", "Aug-2022", "Sep-2022", "Oct-2022", "Nov-2022", "Dec-2022"
-            , "Jan-2023", "Feb-2023", "Mar-2023", "Apr-2023", "May-2023", "Jun-2023", "Jul-2023", "Aug-2023", "Sep-2023", "Oct-2023", "Nov-2023", "Dec-2023"
-
-
-         ],
-         crosshair: true,
-
-         min: 0,
-         max: 11
-      },
-      yAxis: {
-
-         title: {
-            text: "Income (EGP)"
-         }
-      },
-      series: <SeriesOptionsType[]>this.data,
-      plotOptions: {
-
-         column: {
-            pointPadding: 0.2,
-            borderWidth: 0
-         },
-         series: {
-            marker: {
-               radius: 10,
+               },
+               scale: 1,
+               fallbackToExportServer: true
             },
-         },
+            tooltip: {
+               headerFormat: '<span style="font-size:20px;">{point.key}</span><table style="">',
+               pointFormat: '<tr><td style="font-size:15px;font-weight:bold;color:{series.color};padding:0">{series.name}</td>' +
+                  '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
+               footerFormat: '</table>',
+               shared: true,
+               useHTML: true
+            },
+            chart: {
+               type: "column", zoomType: 'x'
+            },
+            title: {
+               text: "Monthly Specialities Income"
+            },
 
-      },
+            xAxis: {
+               zoomEnabled: true,
+               scrollbar: { enabled: true, minWidth: 800 },
+               type: 'datetime',
+               dateTimeLabelFormats: { // don't display the dummy year
+                  month: '%m-%Y',
+                  year: '%m-%Y',
+               },
 
-      navigation: {
+               crosshair: true,
 
-         buttonOptions: {
-            enabled: true,
-            align: 'right',
-            verticalAlign: 'top',
-            y: 0
+               // min: Date.UTC(2020, 4, 12),
+               // max: Date.UTC(2030, 4, 12)
+            },
+            yAxis: {
+
+               title: {
+                  text: "Income (EGP)"
+               },
+               ceiling:50000
+            },
+            series: <SeriesOptionsType[]>this.rebased,
+            plotOptions: {
+
+               column: {
+                  pointPadding: 0.2,
+                  borderWidth: 0
+               },
+               series: {
+                  marker: {
+                     radius: 10,
+                  },
+               },
+
+            },
+
+            navigation: {
+
+               buttonOptions: {
+                  enabled: true,
+                  align: 'right',
+                  verticalAlign: 'top',
+                  y: 0
+               }
+            },
+
+         };
+         
+
+      });
+
+   }
+   createArray() {
+      const rebased: any[] = [];
+      const specialities = this.servUtils.specialities;
+      specialities.forEach((specialitiy) => {
+
+         let obj = {
+            name: specialitiy,
+            data: this.monthlySpecialitiesIncome
+               .filter((entry) => entry.speciality == specialitiy)
+               .map((entry) => { return [ Date.parse(entry.date), entry.total] })
          }
-      },
-
-   };
-
-   constructor() { }
-
+         if(obj.data.length)
+         rebased.push(obj);
+      })
+   return rebased;
+   }
    ngOnInit() {
    }
 }
+
