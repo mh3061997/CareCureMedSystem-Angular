6d71fbbb801c5c36b262af4d440487c84f213d6c
mhaitham <mhaitham@sumerge.com>
2021-08-03 01:26:00 +0200

    Added History fields to interface

--- src/app/interfaces/res-appointment.ts
+++ src/app/interfaces/res-appointment.ts
@@ -16,4 +16,58 @@ export interface ResAppointment {
     invoice?:ResInvoice;
     doctorNotes?:ResNoteAppointment[];
     userLoggerName?:string;
+
+    
+	//History
+	complain?:string;
+	presentIllnessHistory?:string;
+	pastHistory?:string;
+	drugHistory?:string;
+	familyHistory?:string;
+	lumpOrUlcerHistoryDuration?:string;
+	lumpOrUlcerHistoryFirstSymptoms?:string;
+	lumpOrUlcerHistoryOtherSymptoms?:string;
+	lumpOrUlcerHistoryProgression?:string;
+	lumpOrUlcerHistoryMultiplicity?:string;
+	lumpOrUlcerHistoryCauses?:string;
+
+	//Examination
+	generalCondition?:string;
+	vitalData?:string;
+	chestExamination?:string;
+	abdominalExamination?:string;
+	localExaminationForUlcerAndLumpSite?:string;
+	localExaminationForUlcerAndLumpShape?:string;
+	localExaminationForUlcerAndLumpSize?:string;
+	localExaminationForUlcerAndLumpColor?:string;
+	localExaminationForUlcerAndLumpTemperature?:string;
+	localExaminationForUlcerAndLumpTenderness?:string;
+	localExaminationForUlcerAndLumpSurface?:string;
+	localExaminationForUlcerAndLumpAge?:string;
+	localExaminationForUlcerAndLumpConsistence?:string;
+	localExaminationForUlcerAndLumpReducability?:string;
+	localExaminationForUlcerAndLumpRelationToSurroundStructure?:string;
+	localExaminationForUlcerAndLumpLymphDrainage?:string;
+	breastExaminationAbnormalFindingsDuringBreastExam?:string;
+	breastExaminationNippleDischarge?:string;
+	breastExaminationSkinInvolvement?:string;
+	breastExaminationAxillaryMass?:string;
+	breastExaminationBreastMass?:string;
+	breastExaminationMobileRight?:string;
+	breastExaminationSize?:string;
+	neckExamination?:string;
+
+	//Investigations
+	lab?:string;
+	radiology?:string;
+	endoscopic?:string;
+	biopsy?:string;
+
+	//Diagnoses
+	provisionalDiagnoses?:string;
+	finalDiagnosis?:string;
+	planOfManagement?:string;
+	operativeDetails?:string;
+	postOperativeComplication?:string;
+
 }
