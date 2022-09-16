3343e3cd3c859e583507020d1d9d599b32752e7c
Mohamed Haitham <mohamed.haitham@nexocraft.com>
2022-09-16 12:24:53 +0200

    add loading states

--- src/app/components/patient/patient-add-membership-dialog/patient-add-membership-dialog.component.ts
+++ src/app/components/patient/patient-add-membership-dialog/patient-add-membership-dialog.component.ts
@@ -1,9 +1,13 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { ResPackageBase } from 'src/app/interfaces/res-package-base';
 import { ResPatient } from 'src/app/interfaces/res-patient';
 import { ServPackageBaseService } from 'src/app/services/serv-package-base.service';
-import { ResMembership } from 'src/app/interfaces//res-membership'
+import { ResMembership } from 'src/app/interfaces//res-membership';
 import { ServMembershipService } from 'src/app/services/serv-membership.service';
 import { ServUtilitiesService } from 'src/app/services/serv-utilities.service';
 import { ResInvoice } from 'src/app/interfaces/res-invoice';
@@ -14,100 +18,89 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-patient-add-membership-dialog',
   templateUrl: './patient-add-membership-dialog.component.html',
-  styleUrls: ['./patient-add-membership-dialog.component.css']
+  styleUrls: ['./patient-add-membership-dialog.component.css'],
 })
 export class PatientAddMembershipDialogComponent implements OnInit {
-
   packageBases: ResPackageBase[];
   patient: ResPatient;
+  showSpinner = false;
 
-
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { patient: ResPatient },
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { patient: ResPatient },
     public dialogRef: MatDialogRef<PatientAddMembershipDialogComponent>,
     private servPackageBase: ServPackageBaseService,
     private servMemberships: ServMembershipService,
-    private servInvoice : ServInvoiceService,
-    private servInvoiceItem:ServInvoiceItemService,
-    private router :Router) {
-
-    console.log(data.patient);
+    private servInvoice: ServInvoiceService,
+    private servInvoiceItem: ServInvoiceItemService,
+    private router: Router
+  ) {
     this.patient = data.patient;
 
-    servPackageBase.getPackageBasesByStatus("Ongoing").subscribe(
+    servPackageBase.getPackageBasesByStatus('Ongoing').subscribe((packages) => {
+      this.packageBases = packages;
+    });
 
-      packages => {
-        this.packageBases = packages;
-      }
-
-    );
-
-    dialogRef.backdropClick().subscribe(result => {
+    dialogRef.backdropClick().subscribe((result) => {
       dialogRef.close(false);
       // console.log("backclick")
     });
-
-
   }
 
   addMembership(packageBaseChosen: ResPackageBase) {
-
+    this.showSpinner = true;
     const newMembership: ResMembership = {
       code: 0,
       dateSubscriped: new Date().toISOString(),
       usedAmount: 0,
       remainingAmount: packageBaseChosen.unitTotal,
       patient: this.patient,
-      packageBase: packageBaseChosen
+      packageBase: packageBaseChosen,
     };
 
-    this.servMemberships.addMembership(newMembership).subscribe(response => {
+    this.servMemberships.addMembership(newMembership).subscribe((response) => {
       this.CreateInvoice(packageBaseChosen);
 
       this.dialogRef.close(true);
     });
   }
 
-  
-  CreateInvoice(packageBaseChosen:ResPackageBase) {
-
+  CreateInvoice(packageBaseChosen: ResPackageBase) {
     let newInvoice: ResInvoice = {
       code: 0,
       dateCreated: new Date().toISOString(),
       dateFinalized: new Date().toISOString(),
       discount: 0,
       invoiceItems: [],
-      paymentMethod: "",
-      status: "Not Paid",
+      paymentMethod: '',
+      status: 'Not Paid',
       totalAfterDiscount: 0,
       totalDue: 0,
       totalPaid: 0,
       totalRemaining: 0,
-      patientMembershipSubscriber:this.patient,
+      patientMembershipSubscriber: this.patient,
     };
-    
-    this.servInvoice.addInvoice(newInvoice).subscribe(invoiceWithCode => {
 
-     newInvoice = invoiceWithCode;
-      this.CreateInvoiceItem(newInvoice,packageBaseChosen);
-
+    this.servInvoice.addInvoice(newInvoice).subscribe((invoiceWithCode) => {
+      newInvoice = invoiceWithCode;
+      this.CreateInvoiceItem(newInvoice, packageBaseChosen);
     });
-
   }
 
-  private CreateInvoiceItem(newInvoice:ResInvoice,packageBaseChosen:ResPackageBase) {
-    
-    const item :ResInvoiceItem = {
-      code:0,
-      price:packageBaseChosen.price,
-      name:packageBaseChosen.name,
-      invoice:newInvoice
-    }
+  private CreateInvoiceItem(
+    newInvoice: ResInvoice,
+    packageBaseChosen: ResPackageBase
+  ) {
+    const item: ResInvoiceItem = {
+      code: 0,
+      price: packageBaseChosen.price,
+      name: packageBaseChosen.name,
+      invoice: newInvoice,
+    };
     this.servInvoiceItem.addInvoiceItem(item).subscribe(() => {
-      this.router.navigate(['admin','invoice',newInvoice.code]);
+      this.showSpinner = false;
+      this.router.navigate(['admin', 'invoice', newInvoice.code]);
     });
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
